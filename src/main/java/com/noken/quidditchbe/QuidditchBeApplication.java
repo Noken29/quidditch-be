77d9b6b9a6092dd6abf443d824090cb09dae1e05
Noken29 <42448187+Noken29@users.noreply.github.com>
2023-01-30 10:54:41 +0200

    Initial commit

---
+++ src/main/java/com/noken/quidditchbe/QuidditchBeApplication.java
@@ -0,0 +1,13 @@
+package com.noken.quidditchbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuidditchBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuidditchBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noken/quidditchbe/QuidditchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noken.quidditchbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuidditchBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
