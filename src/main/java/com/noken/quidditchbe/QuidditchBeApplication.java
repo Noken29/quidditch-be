18c13401831d81ac7087d7c1b96550d875f98cc7
Noken29 <42448187+Noken29@users.noreply.github.com>
2023-02-02 15:03:38 +0200

    Added swagger documentation

--- src/main/java/com/noken/quidditchbe/QuidditchBeApplication.java
+++ src/main/java/com/noken/quidditchbe/QuidditchBeApplication.java
@@ -1,11 +1,13 @@
 package com.noken.quidditchbe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@OpenAPIDefinition
 public class QuidditchBeApplication {
 
     public static void main(String[] args) {
